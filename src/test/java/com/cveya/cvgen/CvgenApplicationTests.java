0e365dd19ed55997068832ccd4f24aab45bc4d76
Iheb Ouelhazi <ouelhazi3iheb@gmail.com>
2023-05-23 10:50:55 +0100

    initialize project

---
+++ src/test/java/com/cveya/cvgen/CvgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cveya.cvgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
